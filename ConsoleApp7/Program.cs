e91b4a8d3d74644d7f859a3c1de05a07320164a5
Viktor Hendrickx <viktor.hendrickx@immalle.eu>
2018-02-05 09:31:29 +0100

    Met array

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -9,6 +9,38 @@ namespace ConsoleApp7
     {
         static void Main(string[] args)
         {
+            /* List<Tafel> tafels = new List<Tafel>();
+             var rndGen = new Random();
+
+             for(var i = 0; i < 10; i++)
+             {
+                 var w = rndGen.Next(50, 200);
+                 var l = rndGen.Next(50, 200);
+                 tafels.Add(new Tafel(w, l));
+             }
+
+             foreach(var t in tafels)
+             {
+                 t.ShowData();
+             }
+             */
+            Tafel[] tafels = new Tafel[10];
+            var rndGen = new Random();
+            for (var i = 0; i < 10; i++)
+            {
+                var w = rndGen.Next(50, 200);
+                var l = rndGen.Next(50, 200);
+                var t = new Tafel(w, l);
+                tafels[i] = t;
+            }
+
+            foreach (var t in tafels)
+            {
+              
+                t.ShowData();
+            }
+
         }
+
     }
 }
---
+++ ConsoleApp7/Tafel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp7
+{
+    class Tafel
+    {
+           private int Width;
+           private int Length;
+
+        public Tafel(int Width, int Length)
+        {
+            this.Width = Width;
+            this.Length = Length;
+        
+        }
+
+        public void ShowData()
+        {
+            Console.WriteLine("De breedte van deze tafel is {0} cm.", Width);
+            Console.WriteLine("De lengte van deze tafel is {0} cm lang", Length);
+            Console.WriteLine("");
+        }
+    }
+}
